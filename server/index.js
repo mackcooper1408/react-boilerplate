1f47620030398b03f3f7109d3ad80a79ea7e6dac
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-11 17:35:23 -0800

    add api middleware to server

--- server/index.js
+++ server/index.js
@@ -13,9 +13,11 @@ const ngrok =
     : false;
 const { resolve } = require('path');
 const app = express();
+const api = require('./middlewares/api');
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+app.use('/api', api);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
