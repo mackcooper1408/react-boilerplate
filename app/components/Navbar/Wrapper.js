2afbdb720544bb1d86ecacf7aa40857b6e3ab4c3
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-13 23:36:57 -0800

    add wrapper for navbar component

---
+++ app/components/Navbar/Wrapper.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  word-break: normal;
+  width: 100%;
+  background-color: grey;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  overflow: hidden;
+`;
+
+export default Wrapper;
