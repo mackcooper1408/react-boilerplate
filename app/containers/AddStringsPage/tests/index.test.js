e8b8879a602b51cd16674e51e89fe39e14c8631d
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-16 12:59:47 -0800

    update strings page and add strings tests

--- app/containers/AddStringsPage/tests/index.test.js
+++ app/containers/AddStringsPage/tests/index.test.js
@@ -1,18 +1,75 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import { addString } from '../../App/actions';
 
-import AddStringsPage from '../index';
+import { AddStringsPage, mapDispatchToProps } from '../index';
+import { changeStringData } from '../actions';
+import configureStore from '../../../configureStore';
 
 describe('<AddStringsPage />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale="en">
-        <AddStringsPage />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <AddStringsPage loading={false} error={false} />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
+  });
+
+  describe('mapDispatchToProps', () => {
+    describe('handleChange', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.handleChange).toBeDefined();
+      });
+
+      it('should dispatch changeStringData when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const stringData = 'mxstbr';
+        result.handleChange({ target: { value: stringData } });
+        expect(dispatch).toHaveBeenCalledWith(changeStringData(stringData));
+      });
+    });
+
+    describe('handleSubmit', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.handleSubmit).toBeDefined();
+      });
+
+      it('should dispatch addString when called', () => {
+        const preventDefault = jest.fn();
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const evt = { preventDefault };
+        result.handleSubmit(evt);
+        expect(preventDefault).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenCalledWith(addString());
+      });
+
+      // it('should preventDefault if called with event', () => {
+      //   const preventDefault = jest.fn();
+      //   const result = mapDispatchToProps(() => {});
+      //   const evt = { preventDefault };
+      //   result.handleSubmit(evt);
+      //   expect(preventDefault).toHaveBeenCalledWith();
+      // });
+    });
   });
 });
--- app/containers/StringsPage/tests/index.test.js
+++ app/containers/StringsPage/tests/index.test.js
@@ -1,18 +1,73 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
 
-import StringsPage from '../index';
+import { StringsPage, mapDispatchToProps } from '../index';
+
+import { loadStrings } from '../../App/actions';
+import configureStore from '../../../configureStore';
 
 describe('<StringsPage />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale="en">
-        <StringsPage />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <StringsPage loading={false} error={false} strings={[]} />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should fetch the strings on mount', () => {
+    const mockOnLoad = jest.fn();
+    render(
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <StringsPage onLoad={mockOnLoad} />
+        </IntlProvider>
+      </Provider>,
+    );
+    expect(mockOnLoad).toHaveBeenCalled();
+  });
+
+  // TODO: update this
+  xit('should not call onLoad if store contains strings', () => {
+    const mockOnLoad = jest.fn();
+    render(
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <StringsPage onLoad={mockOnLoad} />
+        </IntlProvider>
+      </Provider>,
+    );
+    expect(mockOnLoad).not.toHaveBeenCalled();
+  });
+
+  describe('mapDispatchToProps', () => {
+    describe('onLoad', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.onLoad).toBeDefined();
+      });
+
+      it('should dispatch loadStrings when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        result.onLoad();
+        expect(dispatch).toHaveBeenCalledWith(loadStrings());
+      });
+    });
   });
 });
