996c5c1be66343e3aee36f041362cd462b9ba895
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-16 12:10:11 -0800

    add form styling for add strings page

---
+++ app/containers/AddStringsPage/Button.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 0.8em;
+  text-align: center;
+  padding: 0.15em 0.5em;
+  margin: 0.25em;
+  background-color: transparent;
+  border: 1px solid grey;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.1);
+    background-color: grey;
+    color: white;
+  }
+`;
+
+export default Button;
---
+++ app/containers/AddStringsPage/Form.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  font-size: 2em;
+  margin: 0.8em 0.25em;
+`;
+
+export default Form;
