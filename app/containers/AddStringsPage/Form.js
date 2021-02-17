eb2458ca9243fab2aedb68313eba87bd603e90b6
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-16 22:41:07 -0800

    update Form styling

--- app/containers/AddStringsPage/Form.js
+++ app/containers/AddStringsPage/Form.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 const Form = styled.form`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
+  align-items: center;
   font-size: 2em;
   margin: 0.8em 0.25em;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export default Form;
