072ea8223b50d9d6945997ab0e43a7c351cae89c
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-16 22:46:40 -0800

    update imports and comments

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,11 +7,12 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import LinkItem from '../../components/LinkItem';
-import H1 from '../../components/H1';
-import List from '../../components/List';
+import LinkItem from 'components/LinkItem';
+import H1 from 'components/H1';
+import List from 'components/List';
 import messages from './messages';
 
+// items to be displayed | id used for unique key props
 const items = [
   {
     id: '1',
