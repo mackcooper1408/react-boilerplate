bda6ca3d8f8b292bf8fb0c0509c26951485b5060
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-02-16 22:47:13 -0800

    wrap homepage tests in router / provider

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
+import { ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
 
 import HomePage from '../index';
+import configureStore from '../../../configureStore';
+import history from '../../../utils/history';
 
 describe('<HomePage />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale="en">
-        <HomePage />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <ConnectedRouter history={history}>
+            <HomePage />
+          </ConnectedRouter>
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
   });
